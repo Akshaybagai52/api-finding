7d44189ebe5707bfec3832a9f35da30a9360e310
Akshay Bagai <akshaybagai52@gmail.com>
2023-10-27 16:12:12 +0530

    added

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
 const API_URL = "https://api.publicapis.org/entries";
 
 const Table = () => {
-  const [APIs, setAPIs] = useState([]);
+//   const [APIs, setAPIs] = useState([]);
 
   //   useEffect(() => {
   //     const fetchData = async () => {
